e5a864266082a5601f13a8e8876ac4c67f3a6bc3
naga55 <e145741@ie.u-ryukyu.ac.jp>
2016-12-12 14:56:34 +0900

    pro2 Report5

---
+++ src/main/jp/ac/uryukyu/ie/e145741/report5.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e145741;
+
+/**
+ * Created by e145741 on 2016/12/12.
+ */
+public class report5 {
+
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
